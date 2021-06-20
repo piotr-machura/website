ed16d7d4bda3834aa1fb329d919a9bdba5abf630
Piotr Machura <piotr.machura16@gmail.com>
2021-06-20 13:03:33 +0200

    Simple version almost finished

--- generate.py
+++ generate.py
@@ -1,3 +1,11 @@
+"""Site generation script.
+
+After installing the requirements run `python3 generate.py` to generate the
+website in the ./site directory.
+
+**WARNING:** All contents of ./site will be deleted.
+"""
+
 from os import makedirs
 from os.path import relpath, basename
 from glob import glob
@@ -5,6 +13,7 @@ from datetime import datetime
 from shutil import rmtree, copytree
 from markdown import Markdown
 from jinja2 import Environment, FileSystemLoader as fsl
+# pylint: disable=C0115, C0103, C0116
 
 
 class Document:
@@ -15,7 +24,8 @@ class Document:
             '.md', '.html')
         self.basename = basename(self.path)
         with open(path) as f:
-            md = Markdown(extensions=['meta'])
+            md = Markdown(
+                extensions=['meta', 'extra', 'toc', 'markdown_del_ins'])
             self.content = md.convert(f.read())
             self.meta = md.Meta
         for el in self.meta:
@@ -52,7 +62,7 @@ if __name__ == '__main__':
     home = Document('./src/index.md')
     home.render(template='home.html')
 
-    # Compile blog
+    # Render blog
     articles = glob('./src/blog/*')
     articles.remove('./src/blog/index.md')
     articles = sorted(
@@ -64,7 +74,7 @@ if __name__ == '__main__':
         article.render(template='article.html')
 
     blog = Document('./src/blog/index.md')
-    blog.render(template='blog.html', listing=articles)
+    blog.render(template='listing.html', listing=articles, date=True)
 
     # Render projects
     projects = glob('./src/projects/*')
@@ -78,7 +88,7 @@ if __name__ == '__main__':
         project.render(template='project.html')
 
     proj_idx = Document('./src/projects/index.md')
-    proj_idx.render(template='projects.html', listing=projects)
+    proj_idx.render(template='listing.html', listing=projects)
 
     # Render writing page
     writing = Document('./src/writing/index.md')
---
+++ src/blog/favorite-equation.md
@@ -0,0 +1,32 @@
+Title:        Favorite equation
+Description:  Of course it's the divergence theorem
+Date:         2021-05-18
+
+# Favorite equation
+The **[divergence theorem](https://en.wikipedia.org/wiki/Divergence_theorem)**, also referred to with
+any combination of names *"Gauss"*, *"Green"*, and *"Ostrogradski"*.
+
+$$
+\int\limits_V \nabla \cdot \vec{F} \hspace{5pt} dV = \int\limits_{\partial V} \vec{F} \cdot d\vec{s}
+$$
+
+It's one of those equations where it *looks* like such a cryptic, arbitrary relation, but once you *get it* you know
+that it must obviously be true. Like $1+1=2$ level of true. Arguably the **[generalized Stokes
+theorem](https://en.wikipedia.org/wiki/Generalized_Stokes_theorem)** is prettier, but not as satisfying.
+
+$$
+\int\limits_\Omega d\omega = \int\limits_{\partial\Omega} \omega
+$$
+
+Show this to anyone and they will scratch their head wondering what does the $\int$ sign even mean without some sort of
+$dx$ to go with it (hint: it's hidden in the $\omega$). Not nearly as intuitive.
+
+Close second would be the **[Born rule](https://en.wikipedia.org/wiki/Born_rule)** (Born postulate if you are a
+communist), which is the very reason everyone saying: *"Physicists don't use Akham's Razor, just look at quantum
+mechanics!"* is wrong and cannot be trusted.
+
+$$
+\mathbb{P}(\lambda_n) = \vert \langle n \vert \psi \rangle \vert^2 
+$$
+
+Just look at it. So pretty. And so intuitive.
